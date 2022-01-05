b3db48921108afdcaf9af59943e61a74e0a112e7
Sai Kallepalli <srvarma7@gmail.com>
2022-01-05 23:09:32 +0530

    Initial Commit

---
+++ Shared/ConsumerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumerAppApp.swift
+//  Shared
+//
+//  Created by Sai Kallepalli on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ConsumerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
